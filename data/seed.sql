2814b27b69625f57a941b2f432f80e34b66e6b9e
zakeyah <zakeyah@makesy.com>
2022-11-02 23:40:39 +0300

    email-template

--- data/seed.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO users (email,password,firstName) 
-VALUES('test22@gmail.com',' 2222','user name22222');
--- data/users.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS users;
-CREATE TABLE users (
-  id SERIAL PRIMARY KEY,
-  email VARCHAR(255),
-  password VARCHAR(255),
-  firstName VARCHAR(255)
-  );
